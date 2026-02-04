5b82b91fc5ecfecaff51feef676b6df6af8297df
giorgikh93 <giorgikhatiashvili93@yahoo.com>
2026-02-04 16:36:42 +0400

    add feature flag - DELETE_VAULT_ENABLED

--- src/constants/flags.js
+++ src/constants/flags.js
@@ -3,3 +3,4 @@ export const PASSKEY_SUPPORT_ENABLED = false
 export const AUTO_LOCK_ENABLED = true
 export const BE_AUTO_LOCK_ENABLED = true
 export const SAVE_CREDENTIALS_AFTER_LOGIN_ENABLED = false
+export const DELETE_VAULT_ENABLED = false
