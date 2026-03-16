4a02ba1bb02b0b8a5e4e33b364b785df1d44f1d9
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-03-17 01:18:17 +0400

    Add IPC_SOCKET_DIR_NAME constant for shared socket path resolution.

--- src/constants/nativeMessaging.js
+++ src/constants/nativeMessaging.js
@@ -2,6 +2,8 @@ export const EXTENSION_ID = 'pdeffakfmcdnjjafophphgmddmigpejh'
 
 export const MANIFEST_NAME = 'com.pears.pass'
 
+export const IPC_SOCKET_DIR_NAME = '.pearpass'
+
 export const NATIVE_MESSAGING_BRIDGE_PEAR_LINK_PRODUCTION =
   'pear://i49831s3quatekogbc411cdfmg6xmjt1dycxxr3kt1b1qms5x8ro'
 export const NATIVE_MESSAGING_BRIDGE_PEAR_LINK_STAGING =
